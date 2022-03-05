ed95968f2ad93ab8ecf27e0e66c4b55df8ed4a23
ibeandyy <adhyr4@gmail.com>
2022-03-04 16:59:53 -0800

    initial scuffed commit lesgo

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Game, WireObject } = require("@gathertown/gather-game-client");
+global.WebSocket = require("isomorphic-ws");
+const {
+  storeID,
+  discordToken,
+  gatherAPI,
+  guildID,
+  channelID,
+} = require("./config.json");
+
+//instantiate Gather client, pass in SpaceID (new as of SDk V33.0.2)
+var game = new Game(storeID, () => Promise.resolve({ apiKey: gatherAPI }));
+
+//Require in the Discord wrapper
+const { Client, Intents } = require("discord.js");
+
+//Instantiate Discord Bot, give it way too many intents/privileges
+const client = new Client({ intents: 32767 });
+//Login Discord Bot
+client.login(discordToken);
+//Connect Gather Client
+game.connect();
+//Set Object equal to Gather's active players object
+let object = game.players;
+
+//Listen for the Discord Ready event
+client.on("ready", () => {
+  //Set guild equal to TestCave's guild ID
+  const guild = client.guilds.cache.get(guildID);
+  setInterval(() => {
+    //Iterate through Gather's Player object, push all usernames to an array(x)
+    let x = [];
+    for (let key in object) {
+      x.push(game.players[key].name);
+    }
+    //Define activePlayer count as the length of the array
+    let activePlayers = x.length;
+
+    //Define voice channel we'll be changing
+    const channel = guild.channels.cache.get(channelID);
+    channel.setName(`Gather Online: ${activePlayers}`);
+    console.log(`ActivePlayers: ${activePlayers}`);
+    console.log("Name should be set");
+  }, 60000);
+});
