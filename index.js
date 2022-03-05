0e864e87f460bbc130d1d58e0bd44f7da0bdf090
ibeandyy <adhyr4@gmail.com>
2022-03-05 11:42:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,5 @@ client.on("ready", () => {
     //Define voice channel we'll be changing
     const channel = guild.channels.cache.get(channelID);
     channel.setName(`ConcaveTown: ${activePlayers} Online`);
-    console.log(`ActivePlayers: ${activePlayers}`);
-    console.log("Name should be set");
   }, 60000);
 });
